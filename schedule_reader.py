16dec39d3a216146e71a16a9cb140536445196cb
Hwi Geun Hwang <57276270+hgsy@users.noreply.github.com>
2022-03-20 08:16:12 +0900

    Update schedule_reader.py

--- schedule_reader.py
+++ schedule_reader.py
@@ -1,72 +1,49 @@
 import pandas as pd
 
 Location = 'excel'
-File = 'scadule.xlsx'
+File = 'schedule.xlsx'
 
 data_pd = pd.read_excel('{}/{}'.format(Location, File),
-                     header=None, index_col=None, names=None)
+                        header=None, index_col=None, names=None)
 
-#시간표 수작업이 귀찮지만 머리쓰기도 귀찮은 휘근쨩의 무지성무논리30분코딩 우효ww시간표리더기
-#2021 2학기 시간표 기준
-#시간표 형식이 바뀌면 새로 만들던가 수정 필요
-#무지성 코딩이라 다시 짜는게 편할 수도 있음
+# 시간표 수작업도 싫고 머리쓰기도 귀찮은 휘근쨩의 무지성무논리30분코딩 시간표리더기
+# 2022 1학기 시간표 기준
+# 그냥 다시 만들자 tlqkf ㅋㅋ 하나하나 바꾸기 매우 귀찮음
 
-h=4
-#행
+h = 4
+# 행
 
-y=1
-#열
+y = 1
+# 열
 
 for i in range(0, 24):
-    y=1
-    E = (i%8) + 1
-    #반
+    y = 1
+    E = (i % 8) + 1
+    # 반
 
-    M = (i//8) + 1
-    #학년
-    
-    print('"' +str(M) + "-" + str(E)+ '" : [')
+    M = (i // 8) + 1
+    # 학년
 
-    for j in range(1, 13):
-        if y>12:
+    print('"' + str(M) + "-" + str(E) + '" : [')
+
+    for j in range(1, 15):
+        if y > 14:
             break
 
         else:
-            if y==1:
+            if y == 1:
                 print('["' + data_pd.iloc[h, y] + '", ', end=' ')
                 y = y + 1
 
-            elif y==6:
+            elif y == 7:
                 print('"' + data_pd.iloc[h, y] + '",], ', end=' ')
                 y = y + 1
 
-            elif y==7:
+            elif y == 8:
                 print('["' + data_pd.iloc[h, y] + '", ', end=' ')
                 y = y + 1
 
-            elif y==12:
-                print('"' + data_pd.iloc[h, y] + '",], ', end=' ')
-                y = y + 1
-
-            else:
-                print('"'+data_pd.iloc[h,y]+'", ', end= ' ')
-                y = y + 1
-
-        if j==6:
-            print(" ")
-
-    print(" ")
-
-    for j in range(1, 8):
-        if y>19:
-            break
-
-        else:
-            if y == 13:
-                print('["' + data_pd.iloc[h, y] + '", ', end=' ')
-                y = y + 1
-
-            elif y == 19:
+            elif y == 14:
                 print('"' + data_pd.iloc[h, y] + '",], ', end=' ')
                 y = y + 1
 
@@ -74,27 +51,49 @@ for i in range(0, 24):
                 print('"' + data_pd.iloc[h, y] + '", ', end=' ')
                 y = y + 1
 
-        if j==7:
+        if j == 7:
             print(" ")
 
-    for j in range(1, 13):
-        if y>31:
+    print(" ")
+
+    for j in range(1, 7):
+        if y > 20:
             break
 
         else:
-            if y == 20:
+            if y == 15:
                 print('["' + data_pd.iloc[h, y] + '", ', end=' ')
                 y = y + 1
 
-            elif y == 25:
+            elif y == 20:
                 print('"' + data_pd.iloc[h, y] + '",], ', end=' ')
                 y = y + 1
 
-            elif y == 26:
+            else:
+                print('"' + data_pd.iloc[h, y] + '", ', end=' ')
+                y = y + 1
+
+        if j == 6:
+            print(" ")
+
+    for j in range(1, 15):
+        if y > 34:
+            break
+
+        else:
+            if y == 21:
                 print('["' + data_pd.iloc[h, y] + '", ', end=' ')
                 y = y + 1
 
-            elif y == 31:
+            elif y == 27:
+                print('"' + data_pd.iloc[h, y] + '",], ', end=' ')
+                y = y + 1
+
+            elif y == 28:
+                print('["' + data_pd.iloc[h, y] + '", ', end=' ')
+                y = y + 1
+
+            elif y == 34:
                 print('"' + data_pd.iloc[h, y] + '",] ', end=' ')
                 y = y + 1
 
@@ -102,10 +101,8 @@ for i in range(0, 24):
                 print('"' + data_pd.iloc[h, y] + '", ', end=' ')
                 y = y + 1
 
-        if j==6:
+        if j == 7:
             print("")
-            
-    print("\n],")
-    h = h + 3
-    
 
+    print("\n],")
+    h = h + 2
